3e3698f03c82bd3600dd4d6f25e84b7a2c72a357
minji6313 <minji6313@yahoo.com>
2023-05-24 18:48:52 +0900

    지우기

--- src/study_2/test.java
+++
@@ -1,9 +0,0 @@
-package study_2;
-
-public class test {
-	public static void  main(String[]  args) {
-		System.out.println("zzz");
-		System.out.println("가나라다라ㅏ말");
-		System.out.println("드디어댐");
-	}
-}
