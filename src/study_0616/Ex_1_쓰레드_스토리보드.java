15a66f646d855a77b15693ececde25a8d7220710
minji6313 <minji6313@yahoo.com>
2023-06-16 09:52:42 +0900

    "?"

---
+++ src/study_0616/Ex_1_쓰레드_스토리보드.java
@@ -0,0 +1,14 @@
+package study_0616;
+
+public class Ex_1_쓰레드_스토리보드 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
+/*
+스토리보드란 ?
+1. UX 구현에 수반되는 사용자의 목표, 인터페이스 간 상호작용을 시각화 하여 개발자, 디자이너와의 의사소통을 돕는 도구
+2. 완성해야 할 서비스와 예상되는 사용자 경험을 미리 보기 위한 방법이다.
+*/
