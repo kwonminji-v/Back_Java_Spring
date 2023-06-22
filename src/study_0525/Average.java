896051e5076f8f0ccff4ae1a2fad6e1f089c5766
minji6313 <minji6313@yahoo.com>
2023-06-22 19:37:38 +0900

    "server복습하기"

--- src/study_0525/Average.java
+++ src/study_0525/Average.java
@@ -3,9 +3,9 @@ package study_0525;
 public class Average {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		//선언된 값들의 점수들을 이용해서 평균값을 구하는 코드를 작성해보자.
+		int randomNumber = (int)(Math.random()*6) + 1;
+		System.out.println(randomNumber);
 	}
 
 }
