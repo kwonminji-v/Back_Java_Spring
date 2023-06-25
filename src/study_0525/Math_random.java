4c0bc4c43f555b8063d19d1f9a359e3ef66017d3
minji6313 <minji6313@yahoo.com>
2023-06-25 23:21:47 +0900

    복습

---
+++ src/study_0525/Math_random.java
@@ -0,0 +1,22 @@
+package study_0525;
+
+import java.util.Iterator;
+
+public class Math_random {
+
+	public static void main(String[] args) {
+//		for (int i = 1; i <= 5; i++) {
+//			for (int j = 1; j <= i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		int i =1 , j = 1;
+		
+		while( i <= 10) {
+			System.out.println("*");
+			i++;
+		}
+}
+}
